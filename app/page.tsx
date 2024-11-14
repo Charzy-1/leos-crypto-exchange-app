510bc3e06271d920667d24a0fe5f5dbcdba14e2e
Charles <chazylites@gmail.com>
2024-11-14 15:39:53 +0100

    Added images and icons to the public folder

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <h1 className="h1-bold p-4 text-center">Welcome to Next.JS</h1>
-    </div>
+    <>
+      <h1 className="h1-bold font-space-grotesk p-4">Welcome to Next.JS</h1>
+      <h1 className="h1-bold font-inter p-4 text-center">Welcome to Next.JS</h1>
+    </>
   );
 };
 
