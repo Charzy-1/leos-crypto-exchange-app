b62a16baeb3b5cc1f0f9808ab621fbeeb89c5ff1
Charles <chazylites@gmail.com>
2024-11-28 14:45:26 +0100

    Added the Mobile Nav and Navlink

---
+++ app/(root)/collection/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Collections = () => {
+  return <div>Collections page</div>;
+};
+
+export default Collections;
--- components/navigation/navbar/MobileNavigation.tsx
+++ components/navigation/navbar/MobileNavigation.tsx
@@ -1,17 +1,22 @@
 import {
   Sheet,
+  SheetClose,
   SheetContent,
   SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import Link from "next/link";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import NavLinks from "./NavLinks";
 
 const MobileNavigation = () => {
   return (
     <Sheet>
-      {/* hamburger container */}
+      {/* Hamburger container */}
       <SheetTrigger asChild>
         <Image
           src="/icons/hamburger.svg"
@@ -26,11 +31,44 @@ const MobileNavigation = () => {
         className="background-light900_dark200 border-none"
       >
         <SheetHeader>
-          <SheetTitle>Are you absolutely sure?</SheetTitle>
-          <SheetDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </SheetDescription>
+          <SheetTitle className="hidden">Navigation</SheetTitle>
+          <Link href="/" className="flex items-center gap-1">
+            <Image
+              src="/images/site-logo.svg"
+              width={50}
+              height={50}
+              alt="logo"
+            />
+            <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900">
+              Leos<span className="text-green-500">Exchange</span>
+            </p>
+          </Link>
+
+          <div className="no-scrollbar flex h-[calc(100vh-80px)] flex-col justify-between overflow-y-auto">
+            <SheetClose asChild>
+              <section className="flex h-full flex-col gap-6 pt-16">
+                <NavLinks isMobileNav />
+              </section>
+            </SheetClose>
+
+            <div className="flex flex-col gap-3">
+              <SheetClose asChild>
+                <Link href={ROUTES.SIGN_IN}>
+                  <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                    <span className="primary-text-gradient">Log In</span>
+                  </Button>
+                </Link>
+              </SheetClose>
+
+              <SheetClose asChild>
+                <Link href={ROUTES.SIGN_UP}>
+                  <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
+                    Sign Up
+                  </Button>
+                </Link>
+              </SheetClose>
+            </div>
+          </div>
         </SheetHeader>
       </SheetContent>
     </Sheet>
