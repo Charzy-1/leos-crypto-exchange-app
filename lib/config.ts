a1d570c0959f1470cebfbdf47a20d38c10b06ff4
Charles <chazylites@gmail.com>
2025-03-14 14:15:55 +0100

    Ratelimiting added to my auth pages

--- lib/config.ts
+++ lib/config.ts
@@ -1,6 +1,12 @@
 const config = {
   env: {
     databaseUrl: process.env.DATABASE_URL as string,
+    upstash: {
+      redisUrl: process.env.UPSTASH_REDIS_URL,
+      redisToken: process.env.UPSTASH_REDIS_TOKEN,
+      qstashUrl: process.env.QSTASH_URL,
+      qstashToken: process.env.QSTASH_TOKEN,
+    },
   },
 };
 
